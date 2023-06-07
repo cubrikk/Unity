6a82f6551ba087bbab9dc980455bddf7d20fd38e
cubrikk <podorvannydonater@gmail.com>
2023-06-07 21:16:05 +0300

    updZ

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public Rigidbody2D igrok;
+    public Vector2 direction;
+    private void Start()
     {
-        
+        igrok = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        direction.x = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        igrok.MovePosition(igrok.position + direction * speed);
     }
 }
